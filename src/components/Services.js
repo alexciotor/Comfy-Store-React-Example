8224951491614d824f9a7f9777b8aa81df74684d
alexciotor <ciotor.alex@yahoo.com>
2021-02-12 00:09:45 +0200

    data added to component

--- src/components/Services.js
+++ src/components/Services.js
@@ -3,7 +3,29 @@ import styled from 'styled-components'
 import { services } from '../utils/constants'
 
 const Services = () => {
-  return <h4>services </h4>
+  return <Wrapper>
+  <div className="section-center">
+  <article className="header">
+  <h3>custom furiture 
+ <br/>
+  build only for you</h3>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel delectus et cumque commodi eum voluptates incidunt sapiente repudiandae neque iusto!</p>
+  </article>
+  <div className="services-center">
+  {services.map((service)=>{
+    const {id,icon,title,text}= service
+return <article key={id} className='service'>
+<span className="icon">{icon}</span>
+<h4>{title}</h4>
+<p>{text}</p>
+
+</article>
+
+  })}
+  
+  </div>
+  </div>
+   </Wrapper>
 }
 
 const Wrapper = styled.section`
